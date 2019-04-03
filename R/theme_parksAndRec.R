462e8682ab624d3164eed67ae1fd3baf205ab139
Ryo-N7 <ryonakagawara@gmail.com>
2019-04-04 01:02:14 +0900

    start parks & rec normal + light theme

--- R/theme_parksAndRec.R
+++ R/theme_parksAndRec.R
@@ -1,17 +1,56 @@
 #' @title theme_parksAndRec
 #' @description Parks & Recreation theme
+#' @param font.text text font
+#' @param font.title title font
+#' @param font.legend legend font
 #' @return OUTPUT_DESCRIPTION
 #' @details DETAILS
 #' @rdname theme_parksAndRec
 #' @export
 
-theme_parksAndRec <- function(){
+theme_parksAndRec <- function(font.text = "Champion HTF-Heavyweight",
+                              font.title = "Champion HTF-Heavyweight",
+                              font.legend = "Champion HTF-Heavyweight"){
 
-  theme(text = element_text(family = "Champion HTF-Heavyweight"),
-        axis.text = element_text(size = 8),
-        axis.title.y = element_text(angle = 0, vjust= 0.5),
+  theme(text = element_text(family = font.text),
+        title = element_text(family = font.title),
+        axis.text = element_text(size = 8, color = "grey20"),
+        axis.title = element_text(color = "grey20"),
+        legend.title = element_text(family = font.legend),
         plot.background = element_rect(color = NA, fill = "#CCFFBB"),#238443
         panel.background = element_rect(color = NA, fill = "#238443"), # #1A5E1FFF darkgreen
+        panel.grid.major.x = element_blank())
+
+}
+
+
+#' @title theme_parksAndRec_light
+#' @description Parks & Recreation light theme
+#' @param font.text PARAM_DESCRIPTION, Default: 'Champion HTF-Heavyweight'
+#' @param font.title PARAM_DESCRIPTION, Default: 'Champion HTF-Heavyweight'
+#' @param font.legend PARAM_DESCRIPTION, Default: 'Champion HTF-Heavyweight'
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname theme_parksAndRec_light
+#' @export
+
+theme_parksAndRec_light <- function(font.text = "Champion HTF-Heavyweight",
+                              font.title = "Champion HTF-Heavyweight",
+                              font.legend = "Champion HTF-Heavyweight"){
+
+  theme(text = element_text(family = font.text),
+        title = element_text(family = font.title),
+        axis.text = element_text(size = 8, color = "grey20"),
+        axis.title = element_text(color = "grey20"),
+        legend.title = element_text(family = font.legend),
+        plot.background = element_rect(color = NA, fill = "#238443"),  #238443
+        panel.background = element_rect(color = NA, fill = "#CCFFBB"), # #1A5E1FFF darkgreen
         panel.grid.major.x = element_blank())
 
 }
