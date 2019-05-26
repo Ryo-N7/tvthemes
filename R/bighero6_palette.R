b21781f90419235df9e6411be5f41483457671eb
Ryo-N7 <ryonakagawara@gmail.com>
2019-05-27 00:47:37 +0900

    Big Hero 6 palette!

---
+++ R/bighero6_palette.R
@@ -0,0 +1,50 @@
+bigHero6_palette <- c(
+  "#ffff33",  ## gogo yellow #ffff1a #ffff19
+  "#F84527",  ## honeylemon/fredzilla orange
+  "#4F366F",  ## baymax/hiro darkpurple
+  "#ff0000",  ## baymax red
+  "#00fa9a",  ## wasabi green
+  "#3177D5",  ## fredzilla blue  #50F2F7
+  "#c71585",  ## honey lemon violet #9400d3 #ff00ff #C70039  #c71585
+  "#000000"   ## black
+)
+
+#' @title Big Hero 6 palette
+#' @description Big Hero 6 palette
+#' @inheritDotParams ggplot2::discrete_scale
+#' @seealso
+#'  \code{\link[scales]{manual_pal}}
+#'  [ggplot2::scale_color_discrete]
+#'  [ggplot2::scale_fill_discrete]
+#' @rdname bigHero6_pal
+#' @export
+#' @importFrom scales manual_pal
+
+bigHero6_pal <- function(){
+  scales::manual_pal(bigHero6_palette)
+}
+
+#' @title scale_color_bigHero6
+#' @rdname bigHero6_pal
+#' @export
+#' @importFrom ggplot2 discrete_scale
+
+scale_color_bigHero6 <- function(...){
+  ggplot2::discrete_scale("color", "bigHero6", bigHero6_pal(), ...)
+}
+
+#' @title scale_colour_bigHero6
+#' @rdname bigHero6_pal
+#' @export
+#' @importFrom ggplot2 discrete_scale
+
+scale_colour_bigHero6 <- scale_color_bigHero6
+
+#' @title scale_fill_bigHero6
+#' @rdname bigHero6_pal
+#' @export
+#' @importFrom ggplot2 discrete_scale
+
+scale_fill_bigHero6 <- function(...){
+  ggplot2::discrete_scale("fill", "bigHero6", bigHero6_pal(), ...)
+}
