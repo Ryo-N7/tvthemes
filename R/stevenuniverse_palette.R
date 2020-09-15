d0e83f76107f75704781bc612b7b8d625ef9475d
Ryo-N7 <ryonakagawara@gmail.com>
2020-09-15 23:22:04 +0900

    start steven universe pal. references #13

---
+++ R/stevenuniverse_palette.R
@@ -0,0 +1,324 @@
+## Game of Thrones ----
+
+westeros_palette <- list(
+  ## Steven ----
+  Steven = c(
+    "#cadde1", ## light-skyblue
+    "#7B906F", ## grey-green
+    "#174D79", ## light teal blue
+    "#fffafa", ## snow white
+    "#898989", ## dark-grey
+    "#D4CDB1", ## light-grey-tan
+    "#708090", ## slate grey
+    "#120976", ## purple blue
+    "#000000"  ## black
+  ),
+  ## Garnet ----
+  Garnet = c(
+    "#FF0000", ## rhllor red
+    "#ff8d1a", ## rhllor orange
+    "#FFD700", ## baratheon gold
+    "#b20000", ## fire red
+    "#cc6600", ## baratheon orange
+    "#FFFF66", ## rhllor light yellow
+    "#000000"  ## black
+  ),
+  ## Amethyst ----
+  Amethyst = c(
+    "#89080A", ## maroon
+    "#C5AA73", ## grey
+    "#FEDF25", ## gold
+    "#C24841", ## orange
+    "#8B5B45", ## brown
+    "#000000"  ## black
+  ),
+  ## Pearl ----
+  Pearl = c(
+    "#a6a0f2", ## light purple 80s
+    "#9bf3dc", ## light turqoise 80s
+    "#fad4bf", ## pearl tan 80s
+    "#3e6992", ## pants blue Future
+    "#6deddd", ## light turqoise Future
+    "#f6b6c2", ## light pink Future
+    "#f8c8b1", ## pearl tan Future
+    "#98FB98", ## pale green
+    "#3CB371"  ## medium sea green
+  ),
+  ## Rose Quartz ----
+  RoseQuartz = c(
+    "#AC1F25", ## blood-red
+    "#272727", ## lightblack
+    "#ff9933", ## saffron
+    "#828788", ## metallic grey
+    "#96804b"  ## light-brown
+  ),
+  ## Peridot ----
+  Peridot = c(
+    "#ff8000", ## martell orange
+    "#cc6600", ## martell darkorange
+    "#b81b18", ## martell red
+    "#73110f", ## martell wine red
+    "#f8fc23", ## martell brightyellow
+    "#fafd7b", ## martell lightyellow
+    "#4c2600", ## martell brown
+    "#a57642"  ## martell lightbrown
+  ),
+  ## Lapis Lazuli ----
+  LapisLazuli = c(
+    "#212250", ## darkblue
+    "#AE432F", ## darkorange
+    "#E4EDCA", ## green-white
+    "#8B5B45", ## brown
+    "#4682B4", ## steelblue
+    "#000000"  ## black
+  ),
+  ## Diamonds: White, Yellow, Blue, & Pearl ----
+  Diamonds = c(
+    "#000000", ## black
+    "#708090", ## slate gray
+    "#D4CDB1", ## light-grey-tan
+    "#DCDCDC", ## gainsborough
+    "#F7DC27", ## yellow
+    "#808080"  ## gray
+  ),
+  ##  Manderly ----
+  Jasper = c(
+    "#40e0d0", ## manderly turqouise     #32BB9D
+    "#808080", ## gray
+    "#00ff00", ## light-sea green        #0FBB11
+    "#282828", ## metallic black
+    "#015202", ## dark seaweed green
+    "#195953", ## dark turqoise
+    "#FFFFFF"  ## white
+  ),
+  ##  Arryn ----
+  Topaz = c(
+    "#007fff", ## arryn blue
+    "#7fbfff", ## arryn light blue
+    "#001933", ## arryn dark navy
+    "#4c4cff", ## arryn purple
+    "#ffefb2", ## arryn beige
+    "#a89797", ## arryn lightbrown
+    "#FFFFFF"  ## white
+  ),
+  ##  Arryn ----
+  Spinel = c(
+    "#007fff", ## arryn blue
+    "#7fbfff", ## arryn light blue
+    "#001933", ## arryn dark navy
+    "#4c4cff", ## arryn purple
+    "#ffefb2", ## arryn beige
+    "#a89797", ## arryn lightbrown
+    "#FFFFFF"  ## white
+  ),
+  ##  Arryn ----
+  Bismuth = c(
+    "#007fff", ## arryn blue
+    "#7fbfff", ## arryn light blue
+    "#001933", ## arryn dark navy
+    "#4c4cff", ## arryn purple
+    "#ffefb2", ## arryn beige
+    "#a89797", ## arryn lightbrown
+    "#FFFFFF"  ## white
+  ),
+  ##  Arryn ----
+  Ruby = c(
+    "#007fff", ## arryn blue
+    "#7fbfff", ## arryn light blue
+    "#001933", ## arryn dark navy
+    "#4c4cff", ## arryn purple
+    "#ffefb2", ## arryn beige
+    "#a89797", ## arryn lightbrown
+    "#FFFFFF"  ## white
+  ),
+  ##  Arryn ----
+  Sapphire = c(
+    "#007fff", ## arryn blue
+    "#7fbfff", ## arryn light blue
+    "#001933", ## arryn dark navy
+    "#4c4cff", ## arryn purple
+    "#ffefb2", ## arryn beige
+    "#a89797", ## arryn lightbrown
+    "#FFFFFF"  ## white
+  ),
+  ##  Arryn ----
+  Emerald = c(
+    "#007fff", ## arryn blue
+    "#7fbfff", ## arryn light blue
+    "#001933", ## arryn dark navy
+    "#4c4cff", ## arryn purple
+    "#ffefb2", ## arryn beige
+    "#a89797", ## arryn lightbrown
+    "#FFFFFF"  ## white
+  ),
+  ##  Arryn ----
+  Nephrite = c(
+    "#007fff", ## arryn blue
+    "#7fbfff", ## arryn light blue
+    "#001933", ## arryn dark navy
+    "#4c4cff", ## arryn purple
+    "#ffefb2", ## arryn beige
+    "#a89797", ## arryn lightbrown
+    "#FFFFFF"  ## white
+  ),
+  ##  Arryn ----
+  Aquamarine = c(
+    "#007fff", ## arryn blue
+    "#7fbfff", ## arryn light blue
+    "#001933", ## arryn dark navy
+    "#4c4cff", ## arryn purple
+    "#ffefb2", ## arryn beige
+    "#a89797", ## arryn lightbrown
+    "#FFFFFF"  ## white
+  ),
+  ##  Arryn ----
+  Padparadscha = c(
+    "#007fff", ## arryn blue
+    "#7fbfff", ## arryn light blue
+    "#001933", ## arryn dark navy
+    "#4c4cff", ## arryn purple
+    "#ffefb2", ## arryn beige
+    "#a89797", ## arryn lightbrown
+    "#FFFFFF"  ## white
+  ),
+  ##  Arryn ----
+  Rutile = c(
+    "#007fff", ## arryn blue
+    "#7fbfff", ## arryn light blue
+    "#001933", ## arryn dark navy
+    "#4c4cff", ## arryn purple
+    "#ffefb2", ## arryn beige
+    "#a89797", ## arryn lightbrown
+    "#FFFFFF"  ## white
+  ),
+  ##  Arryn ----
+  Rhodonite = c(
+    "#007fff", ## arryn blue
+    "#7fbfff", ## arryn light blue
+    "#001933", ## arryn dark navy
+    "#4c4cff", ## arryn purple
+    "#ffefb2", ## arryn beige
+    "#a89797", ## arryn lightbrown
+    "#FFFFFF"  ## white
+  ),
+  ##  Arryn ----
+  Flourite = c(
+    "#007fff", ## arryn blue
+    "#7fbfff", ## arryn light blue
+    "#001933", ## arryn dark navy
+    "#4c4cff", ## arryn purple
+    "#ffefb2", ## arryn beige
+    "#a89797", ## arryn lightbrown
+    "#FFFFFF"  ## white
+  )
+)
+
+#' @title Great Houses of Westeros palette
+#' @description Houses Stark, Lannister, Tyrell, Targaryen,
+#' Tully, Greyjoy, Manderly, Martell, Stannis Baratheon, & Arryn
+#' @inheritDotParams ggplot2::discrete_scale
+#' @param palette name of palette, Default: "Stark"
+#' @param n number of colors
+#' @param type discrete or continuous
+#' @param reverse reverse order, Default: FALSE
+#' @rdname westeros_pal
+#' @export
+#' @examples
+#' library(scales)
+#' show_col(westeros_pal(palette = "Stark")(5))
+#' show_col(westeros_pal(palette = "Stannis")(5))
+#' @importFrom scales manual_pal
+#' @importFrom glue glue
+#' @importFrom grDevices colorRampPalette
+
+westeros_pal <- function(palette = "Stark", n,
+                         type = c("discrete", "continuous"),
+                         reverse = FALSE) {
+  westeros <- westeros_palette[[palette]]
+
+  if (reverse == TRUE) {
+    westeros <- rev(westeros)
+  }
+
+  if (missing(n)) {
+    n <- length(westeros)
+  }
+
+  type <- match.arg(type)
+
+  if (type == "discrete" && n > length(westeros)) {
+    stop(glue::glue("Palette does not have {n} colors, maximum is {length(westeros)}!"))
+  }
+
+  westeros <- switch(type,
+                     continuous = grDevices::colorRampPalette(westeros)(n),
+                     discrete = westeros[1:n])
+
+  westeros <- scales::manual_pal(westeros)
+
+  return(westeros)
+}
+
+#' @title scale_color_westeros
+#' @rdname westeros_pal
+#' @export
+#' @examples
+#'
+#' library(ggplot2)
+#' ggplot(airquality, aes(x = Day, y = Temp,
+#'      group = as.factor(Month), color = as.factor(Month))) +
+#'      geom_point(size = 2.5) +
+#'      scale_color_westeros(palette = "Stark")
+#'
+#' ggplot(airquality, aes(x = Day, y = Temp,
+#'      group = as.factor(Month), color = as.factor(Month))) +
+#'      geom_point(size = 2.5) +
+#'      scale_color_westeros(palette = "Stannis")
+#' @importFrom ggplot2 discrete_scale scale_color_gradientn
+
+scale_color_westeros <- function(palette = "Stark", n, type = "discrete",
+                                 reverse = FALSE, ...){
+  if (type == "discrete") {
+    ggplot2::discrete_scale("color", "westeros",
+                            westeros_pal(palette = palette, n = n, type = type,
+                                         reverse = reverse), ...)
+  } else { ## needs work...
+    ggplot2::scale_color_gradientn(colors = westeros_pal(palette = palette, n = n, type = type,
+                                                         reverse = reverse)(8))
+  }
+}
+
+#' @title scale_colour_westeros
+#' @rdname westeros_pal
+#' @export
+#' @examples
+#'
+#' ggplot(airquality, aes(x = Day, y = Temp,
+#'      group = as.factor(Month), color = as.factor(Month))) +
+#'      geom_point(size = 2.5) +
+#'      scale_colour_westeros(palette = "Stannis")
+#' @importFrom ggplot2 discrete_scale scale_color_gradientn
+
+scale_colour_westeros <- scale_color_westeros
+
+#' @title scale_fill_westeros
+#' @rdname westeros_pal
+#' @export
+#' @examples
+#'
+#' ggplot(mpg, aes(displ)) +
+#'      geom_histogram(aes(fill = class), col = "black", size = 0.1) +
+#'      scale_fill_westeros(palette = "Stannis")
+#' @importFrom ggplot2 discrete_scale scale_fill_gradientn
+
+scale_fill_westeros <- function(palette = "Stark", n, type = "discrete",
+                                reverse = FALSE, ...){
+  if (type == "discrete") {
+    ggplot2::discrete_scale("fill", "westeros",
+                            westeros_pal(palette = palette, n = n, type = type,
+                                         reverse = reverse), ...)
+  } else { ## needs work...
+    ggplot2::scale_fill_gradientn(colors = westeros_pal(palette = palette, n = n, type = type,
+                                                        reverse = reverse)(8))
+  }
+}
